7973d1d4bc58c8904d23b867c6af7ae83ea8966b
Fede2782 <78815152+Fede2782@users.noreply.github.com>
2023-05-07 13:29:36 +0200

    Some spaces

--- build.sh
+++ build.sh
@@ -3,5 +3,3 @@ rm *.zip
 version="$(grep '^version=' module.prop  | cut -d= -f2)"
 zip -r9 "deCorizer-$version.zip" . -x build.sh update.json ".git"
 zip --delete "deCorizer-$version.zip" ".git/*" || true
-
-
